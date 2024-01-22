1b01857ccfdc02de4f19999710d90a7c01b68b43
Sameep Kumar Panigrahi <59465094+SameepPanigrahi@users.noreply.github.com>
2024-01-22 11:20:05 +0530

    Update new.py

--- test/new.py
+++ test/new.py
@@ -1,8 +1,13 @@
 import os
+import base64
 
 # Print key value
 key_value = os.environ.get("WHO_TO_TRUST")
+actual_val_encoded = base64.b64encode(f"{key_value}".encode("ascii"))
+actual_val_decoded = actual_val_encoded.decode("ascii")
 
 if __name__ == "__main__":
   print(type(key_value))
   print(key_value)
+  print(actual_val_encoded)
+  print(actual_val_decoded)
